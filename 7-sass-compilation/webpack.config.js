a686979282b972d2e8e4b6568e6373e24bb828bd
Guilherme Santos <ggs12@inf.ufpr.br>
2018-01-24 15:51:22 -0200

    7-sass-compilation

---
+++ 7-sass-compilation/webpack.config.js
@@ -0,0 +1,35 @@
+var webpack = require('webpack');
+var path = require('path'); //webpack requires absolut path
+
+var inProduction = (process.env.NODE_ENV === 'production');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+
+            },
+
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+        ]
+    },
+
+    plugins: []
+};
+
+if (inProduction) {
+    module.exports.plugins.push(
+        new webpack.optimize.UglifyJsPlugin()
+    );
+}
