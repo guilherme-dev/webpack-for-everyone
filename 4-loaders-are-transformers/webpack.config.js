0a2e4c6be7af8aaf2f6fefe9095abf47ae58f567
Guilherme Santos <ggs12@inf.ufpr.br>
2018-01-24 01:22:38 -0200

    4-loaders-are-transformers

---
+++ 4-loaders-are-transformers/webpack.config.js
@@ -0,0 +1,18 @@
+var webpack = require('webpack');
+var path = require('path'); //webpack requires absolut path
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
+};
